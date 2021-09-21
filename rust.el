f43f61f81061037e38ca1bfdfeadf04fde572e3d
Richard Watts <rrw@semiramis.org.uk>
2021-09-21 22:54:44 +0100

    Enable popup docs, disable the annoying minibuffer popups.

--- rust.el
+++ rust.el
@@ -47,8 +47,8 @@
   :custom
   ;; what to use when checking on-save. "check" is default, I prefer clippy
   (lsp-rust-analyzer-cargo-watch-command "clippy")
-  (lsp-eldoc-render-all t)
-  (lsp-idle-delay 0.6)
+  ;(lsp-eldoc-render-all t)
+  ;(lsp-idle-delay 0.6)
   (lsp-rust-analyzer-server-display-inlay-hints t)
   :config
   (add-hook 'lsp-mode-hook 'lsp-ui-mode))
@@ -58,8 +58,9 @@
   :commands lsp-ui-mode
   :custom
   (lsp-ui-peek-always-show t)
+  (lsp-ui-peek-show-directory t)
   (lsp-ui-sideline-show-hover t)
-  (lsp-ui-doc-enable nil))
+  (lsp-ui-doc-enable t))
 
 
 ;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
@@ -80,6 +81,8 @@
 
 (use-package company
   :ensure
+  :custom
+  (company-idle-delay 5)
   :bind
   (:map company-active-map
               ("C-n". company-select-next)
